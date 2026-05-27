c33c9caa9070b9f5f1955075e9ab79578935476a
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2026-05-27 23:44:36 +0300

    Remove key and fix Dockerfile for terminal

--- apps/terminal/src/agents/claude.ts
+++ apps/terminal/src/agents/claude.ts
@@ -18,6 +18,6 @@ export const setup = ({
 
   return {
     tag: AgentTag.ClaudeCode,
-    cmd: `ANTHROPIC_AUTH_TOKEN=f584d58a91394ab69872d2875eff6562.U9Na7jVAKZ3As1fipjW3f0g2 ANTHROPIC_BASE_URL=https://ollama.com ANTHROPIC_API_KEY=""CLAUDE_CODE_ADDITIONAL_DIRECTORIES_CLAUDE_MD=1 claude --print --verbose --output-format stream-json --model ${shellQuote(model)} --add-dir "/${configPath}" --settings "/${settingsPath}" -p`,
+    cmd: `CLAUDE_CODE_ADDITIONAL_DIRECTORIES_CLAUDE_MD=1 claude --print --verbose --output-format stream-json --model ${shellQuote(model)} --add-dir "/${configPath}" --settings "/${settingsPath}" -p`,
   };
 };
